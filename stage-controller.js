8dfc4733a7a4a92a5339c0b1222bc5d155e32df5
jinanbou <scratch.jinanbou@gmail.com>
2025-07-07 11:49:20 +0900

    Update stage-controller.js

--- stage-controller.js
+++ stage-controller.js
@@ -24,6 +24,7 @@ function showStageImage(index) {
 
 // パネル3完了通知を受け取り、ボタンを表示
 window.addEventListener("panel3-drawn", () => {
+  console.log("panel3-drawn event caught");
   showStageButtons();
 });
 
